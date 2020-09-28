b057b73ea43062dd77af325a7f67b79c0b94f643
Uzomeziem Eze <uzoeze1016@gmail.com>
2020-09-28 10:17:14 -0700

    save

--- setup.py
+++ setup.py
@@ -4,8 +4,14 @@ A collection of Data Science helper functions
 
 import setuptools
 
-with open("README.md", 'r') as file:
-    LONG_DESCRIPTION = file.read()
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "twine"
+    ]
+
+with open("README.md", 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
     name='lambdata-thecodinguru',
@@ -13,13 +19,14 @@ setuptools.setup(
     author='thecodinguru',
     description='A collection of Data Science helper functions',
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
+    long_description_content='text/markdown',
     url='https://github.com/thecodinguru/lambdata',
     packages=setuptools.find_packages(),
-    python_requires='>==3.7',
+    python_requires='>=3.8',
+    install_requires=REQUIRED,
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
