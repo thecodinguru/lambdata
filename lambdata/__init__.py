6281a9a30bdc0427a20e97c2f835628d04ac4f5a
Uzomeziem Eze <uzoeze1016@gmail.com>
2020-09-28 10:41:16 -0700

    save

--- lambdata/__init__.py
+++ lambdata/__init__.py
@@ -1,2 +1,37 @@
 import pandas as pd
 import numpy as np
+
+def wrangle(df):
+
+  df = df.copy()
+
+  #Drop high cardinality columns to prevent data leakage in the future
+  df = df.drop(columns=['LandAverageTemperatureUncertainty', 'LandMaxTemperatureUncertainty',
+                        'LandMinTemperatureUncertainty', 'LandAndOceanAverageTemperatureUncertainty' ], axis=1)
+
+  #convert temps from celsius to fahrenheit so it more readable for Americans 
+  def convertTemp(x):
+    x = (x * 1.8) + 32
+    return float(x)
+
+  #apply function to each column
+  df['LandAverageTemperature'] = df['LandAverageTemperature'].apply(convertTemp)
+  df['LandMaxTemperature'] = df['LandMaxTemperature'].apply(convertTemp)
+  df['LandMinTemperature'] = df['LandMinTemperature'].apply(convertTemp)
+  df['LandAndOceanAverageTemperature'] = df['LandAndOceanAverageTemperature'].apply(convertTemp)
+
+  #convert dt column to datetime
+  df["dt"] = pd.to_datetime(df["dt"])
+  df["Month"] = df["dt"].dt.month
+  df["Year"] = df["dt"].dt.year
+  df = df.drop("dt", axis = 1)
+  df = df.drop("Month", axis=1)
+  df = df[df.Year >= 1850]
+
+  #set index to Year column
+  df = df.set_index(['Year'])
+
+  #drop NaN values
+  df = df.dropna()
+
+  return df
